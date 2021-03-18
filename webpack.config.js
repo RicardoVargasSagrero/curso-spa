f995f8333c546ecedbfc264f4d242d4a51cef170
Ricardo Vargas <r.sagrero1@gmail.com>
2021-03-18 16:32:43 -0600

    Se ha creado la página de error 404

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  resolve: {
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        /* Valor que vamos a filtrar de los archivos que estamos utilizando */
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ]
+  },
+  plugins:  [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        /* Donde se encuentra el temple base */
+        template: './public/index.html',
+        filename: './index.html'
+      }
+    ]),
+  ]
+};

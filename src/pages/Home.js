bcc3596bf2304aaa5c6c42a84e5c0de3732afb9d
Ricardo Vargas <r.sagrero1@gmail.com>
2021-03-19 02:04:39 -0600

    Se añade configuración de travis y fiex de typo en Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,14 @@ const Home = async () => {
   const view = `
     <div class = "Characters">
       ${characters.results.map(character => `
-        <article class="Character-item">
+        <article class="Characters-item">
           <a href="#/${character.id}/">
             <img src="${character.image}" alt="${character.name}">
             <h2>${character.name}</h2>
           </a>
         </article>
       `).join('')}
-      <article class="Character-item">
+      <article class="Characters-item">
         <a href="#/1/">
           <img src="image" alt="name">
           <h2>Name</h2>
